8b72b0a15158659463b92fe3300238bb92bf21c0
gueverstarstone <josephmungai028@gmail.com>
2025-12-09 21:42:00 +0300

    first commit

---
+++ day1/first_code.py
@@ -0,0 +1,41 @@
+print('gueverstarstone')
+print('Hello. Wolrd') # first python code
+print('My favorite colors are:', 'blue', 'green', 'red')
+
+#integer
+my_integer_var = 20
+print('my integer is:', my_integer_var)
+
+#float
+number_float = 4.9
+print(number_float,'is a float number.')
+
+#complex-A number with a real and imaginary part, like 6 + 7j.
+complex_num = 6+7j
+print('this', complex_num ,'is a complex number')
+
+#set-An unordered collection of unique elements, like {4, 2, 0}.
+set_of_numbers = {2,5,7}
+print(set_of_numbers)
+
+#disctionary-A collection of key-value pairs enclosed in curly braces, like {'name': 'John Doe', 'age': 28}.
+my_dictionary = {'name': 'gueverstarstone', 'age': 25}
+my_dictionary['age']= 40
+print(my_dictionary)
+
+#tuple-An immutable ordered collection, enclosed in brackets, like (7, 8, 4).
+a_tuple = (5,6,7)
+print('an ordered tuple',a_tuple)
+
+#range- A sequence of numbers, often used in loops, for example, range(5).
+my_range = range(14)
+print('range is:', my_range)
+
+#list-An ordered collection of elements that supports different data types.
+my_list = [1,'Joe', True, 3.14]
+print(my_list)
+
+#none-A special value that represents the absence of a value.
+my_none = None
+print(my_none)
+
