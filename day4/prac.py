14cefebe654f0f6df1f3f48635e1240893cb3e5b
gueverstarstone <josephmungai028@gmail.com>
2025-12-16 21:46:43 +0300

    did some small practice on python funcs

--- day4/prac.py
+++ day4/prac.py
@@ -1,2 +1,31 @@
-my_new_name = input('please type your name.')
-print(f'Welcome, {my_new_name}')
+def name():
+    print('my name is mungai')
+name()
+
+def my_func(a,b):
+    return a + b
+addition = my_func(5,5)
+print(addition)
+
+
+def my_num():
+    my_var = 'welcome'
+    def my_num2():
+        print(my_var)
+
+    my_num2()
+my_num()
+
+def my_num4():
+    my_var2 = 'Hello'
+    my_var3 = ''
+
+
+    def my_num3():
+        nonlocal my_var3
+        my_var3 = 'How are you?'
+        print(my_var2)
+
+    my_num3()
+    print(my_var3)
+my_num4()
