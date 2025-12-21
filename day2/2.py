735b004c9c5ac28f3487b02b65ce150361980f20
gueverstarstone <josephmungai028@gmail.com>
2025-12-21 13:50:21 +0300

    made output comments on the print section

--- day2/2.py
+++ day2/2.py
@@ -1,15 +1,14 @@
 my_str = 'Hello Mungai, "the girl said"'
-print('Hello' in my_str)
+print('Hello' in my_str)  # True
 
-#Escape the single or double quotation mark in the string with a backslash (\)
+# Escape the single or double quotation mark in the string with a backslash (\)
 my_message = 'Hello Mungai, "the girl said, it\'s raining"'
-print(my_message)
+print(my_message)  # Hello Mungai, "the girl said, it's raining"
 
-#string concatenation using str() function
+# string concatenation using str() function
 name = 'Gueverstarstone'
 age = 26
-print(name + str(age))
-
+print(name + str(age))  # Gueverstarstone26
 
 # augmented assignment operator for concatenation. 
 # This is represented by a plus and equals sign (+=), and performs both concatenation and assignment in one step. Here's it in action:
@@ -18,59 +17,51 @@ year = 2006
 
 make_and_year = make
 make_and_year += str(year)
-print(make_and_year)
+print(make_and_year)  # subaru2006
 
-#f-strings
+# f-strings
 # It allows you to handle interpolation and also do some concatenation with a compact and readable syntax.
 school = 'jeremy high'
 year = 2007
 name = 'zuri'
 
 # getting the length of a string using the len() function
-print(len(school))
+print(len(school))  # 11
 
 name_and_year = f'my namae is {name} and i was born in {year}'
-print(name_and_year)
-print(len(name))
+print(name_and_year)  # my namae is zuri and i was born in 2007
+print(len(name))  # 4
 
 my_num = 2
 my_number = 3
 my_num_and_my_number = f'the sum of {my_num} and {my_number} is {my_num + my_number}'
-print(my_num_and_my_number)
+print(my_num_and_my_number)  # the sum of 2 and 3 is 5
 
 # indexing- accessing a character by its index using bracket notation
 my_name = 'zuri muthoni'
-print(my_name[3])
-print(my_name[7])
-print(my_name[-1]) #getting the last character with -1
+print(my_name[3])   # i
+print(my_name[7])   # t
+print(my_name[-1])  # i (last character)
 
 # slicing-String slicing lets you extract a portion of a string or work with only a specific part of it
 last_name = 'peter'
-print(last_name[1:4]) # 1  is the strt index, 4 is the stop index
+print(last_name[1:4])  # ete
 # stop index is non-inclusive
 
-
 # step-used to specify the increment between each index in the slice.
-# Here's the syntax for that:
 # string[start:stop:step]
-# In the example below, the slicing starts at index 0, stops before 11, and extracts every second character:
-
 my_str = 'Hello world'
 print(my_str[0:11:2])  # Hlowrd
 
-# A helpful trick you can do with the step parameter is to reverse a string by setting step to -1, and leaving start and stop blank:
-
+# reverse a string by setting step to -1
 my_str = 'Hello world'
-print(my_str[::-1]) # dlrow olleH
+print(my_str[::-1])  # dlrow olleH
 
-# It can also be helpful to check if a character or set of characters exist in a string before slicing it. 
-# To do that, Python provides the in operator, which returns a boolean that specifies whether the character or characters exist in the string or not.
-
+# check if a character or set of characters exist in a string
 my_str = 'Hello world'
 
 print('Hello' in my_str)  # True
 print('hey' in my_str)    # False
-print('hi' in my_str)    # False
-print('e' in my_str)  # True
-print('f' in my_str)  # False
-
+print('hi' in my_str)     # False
+print('e' in my_str)      # True
+print('f' in my_str)      # False
--- day2/string_methods.py
+++ day2/string_methods.py
@@ -1,37 +1,37 @@
 # upper() method- Returns a new string with all characters converted to uppercase.
 
 first_name = 'Zuriel'
-print(first_name.upper())
+print(first_name.upper())  # ZURIEL
 
 # lower() method- Returns a new string with all characters converted to lowercase.
 second_name = 'MuthoNi'
-print(second_name.lower())
+print(second_name.lower())  # muthoni
 
 # strip() - Returns a new string with the specified leading and trailing characters removed. 
 # If no argument is passed it removes leading and trailing whitespace.
 last_name = ' wanTam ,  ' 
-print(last_name.strip())
+print(last_name.strip())  # wanTam ,
 
 # replace(old, new): Returns a new string with all occurrences of old replaced by new.
 my_str = 'Welcome Mungai'
 new_str = my_str.replace('Welcome', 'Hi')
-print(new_str)
+print(new_str)  # Hi Mungai
 
 # split(separator) - Splits a string on a specified separator into a list of strings. 
 # If no separator is specified, it splits on whitespace.
 split_words = 'Welcome Home'
 new_split_words = split_words.split()
-print(new_split_words)
+print(new_split_words)  # ['Welcome', 'Home']
 
 # split(separator)- with a custom separator
 split_words = 'Welcome Home'
 new_split_words = split_words.split(",")
-print(new_split_words)
+print(new_split_words)  # ['Welcome Home']
 
-#join(iterable): Joins elements of an iterable into a string with a separator.
+# join(iterable): Joins elements of an iterable into a string with a separator.
 my_list = ['welcome', 'home']
 joined_my_str = ' '.join(my_list)
-print(joined_my_str)
+print(joined_my_str)  # welcome home
 
 # startswith(prefix): Returns a boolean indicating if a string starts with the specified prefix.
 my_str = 'hello world'
@@ -43,19 +43,19 @@ my_str = 'hello world'
 ends_with_world = my_str.endswith('world')
 print(ends_with_world)  # True
 
-#find(substring): Returns the index of the first occurrence of substring, or -1 if it doesn't find one.
+# find(substring): Returns the index of the first occurrence of substring, or -1 if it doesn't find one.
 my_str = 'hello world'
 world_index = my_str.find('world')
 print(world_index)  # 6
 
-#count(substring): Returns the number of times a substring appears in a string.
+# count(substring): Returns the number of times a substring appears in a string.
 my_str = 'hello world'
 o_count = my_str.count('l')
 print(o_count)  # 3
 
 # capitalize(): Returns a new string with the first character capitalized and the other characters lowercased.
 my_str = 'rigathiless'
-print(my_str.capitalize())
+print(my_str.capitalize())  # Rigathiless
 
 # isupper(): Returns True if all letters in the string are uppercase and False if not.
 my_str = 'hello world'
